a24240d8e4695aa4181c8304596d6119d8132aa5
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2018-10-12 12:25:26 +0300

    "Main tyhjennetty, pelkka spark jaljella

--- src/main/java/tikape/Main.java
+++ src/main/java/tikape/Main.java
@@ -16,17 +16,22 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
  */
 
 public class Main {
+    //kurssit-ja-kysymykset.herokuapp.com
     
     public static void main(String[] args) throws Exception {
-        Connection c = DriverManager.getConnection("jdbc:sqlite:testi.db");
-        Statement stmt = c.createStatement();
-        ResultSet resultSet = stmt.executeQuery("SELECT 1");
-        
-        if (resultSet.next()) {
-            System.out.println("toimii");
-        } else {
-            System.out.println("virhetta pukkaa");
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
+        
+        //Connection c = DriverManager.getConnection("jdbc:sqlite:testi.db");
+        //Statement stmt = c.createStatement();
+        //ResultSet resultSet = stmt.executeQuery("SELECT 1");
+        
+        //if (resultSet.next()) {
+        //    System.out.println("toimii");
+        //} else {
+        //    System.out.println("virhetta pukkaa");
+        //}
         
         Spark.get("*", (req, res) -> {
             HashMap map = new HashMap<>();
